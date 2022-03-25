e5815359d3694e1f2f99cb4419017c7ee8cd3775
Li Lin <guru.lin@gmail.com>
2022-03-25 22:29:34 +0800

    reformat

--- core_banking/models.py
+++ core_banking/models.py
@@ -17,23 +17,27 @@ class FundTransferRequest(BaseModel):
 
 class FundTransfer(BaseModel):
     transaction_id: str
+
     debit_customer_id: str
     debit_account_id: str
-    currency: str
-    credit_customer_id: str
-    credit_account_id: str
-    transfer_amount: Decimal
-    memo: str
-    transaction_date: str
     debit_prev_balance: Decimal
     debit_prev_avail_balance: Decimal
     debit_balance: Decimal
     debit_avail_balance: Decimal
+
+    credit_customer_id: str
+    credit_account_id: str
     credit_prev_balance: Decimal
     credit_prev_avail_balance: Decimal
     credit_balance: Decimal
     credit_avail_balance: Decimal
+
+    transfer_amount: Decimal
+    currency: str
+    memo: str
+    transaction_date: str
     status: str
+
     limits_req_id: str
 
 
